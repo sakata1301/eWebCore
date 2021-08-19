04d01fe84fef1684f9babdf12e026caf17e02490
sakata1301 <sakatahuyn@gmail.com>
2021-08-19 10:27:25 +0700

    create database

---
+++ eWebCore.Data/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using eWebCore.Data.Entities;
+using eWebCore.Data.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eWebCore.Data.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Categories");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).UseIdentityColumn();
+
+
+            builder.Property(x => x.Status).HasDefaultValue(Status.Active);
+        }
+    }
+}
